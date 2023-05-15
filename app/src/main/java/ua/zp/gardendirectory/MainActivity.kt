cb9152d806df79ca4f97ae6dbcc7ded733170b43
Maksim.Kalitsev <maksim.kalitsev@ukr.net>
2023-05-15 12:30:43 +0300

    add SplashScreen API

--- app/src/main/java/ua/zp/gardendirectory/MainActivity.kt
+++ app/src/main/java/ua/zp/gardendirectory/MainActivity.kt
@@ -1,13 +1,17 @@
 package ua.zp.gardendirectory
 
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import ua.zp.gardendirectory.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        val splashScreen = installSplashScreen()
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
